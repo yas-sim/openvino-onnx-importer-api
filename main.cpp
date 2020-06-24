ec735ed6ac5f9dc2c37fb8934fcaac5a81ed8d22
Yasunori Shimura <yasu0710@gmail.com>
2020-06-24 18:08:17 +0900

    updated main.cpp

--- main.cpp
+++ main.cpp
@@ -7,8 +7,6 @@
 #include <inference_engine.hpp>
 
 #include <ngraph/ngraph.hpp>
-#include <ngraph/opsets/opset.hpp>
-#include <ngraph/opsets/opset1.hpp>
 #include <ngraph/frontend/onnx_import/onnx.hpp>
 
 using namespace std;
@@ -39,7 +37,7 @@ int main(void) {
     ie::Core ie;
 
     // Wraps nGraph model with IE::CNNNetwork
-    std::cout << "Converting an nGraph model into CNNNetwork" << std::endl;
+    std::cout << "Converting an ONNX model into CNNNetwork" << std::endl;
     ie::CNNNetwork network(ng_function);
 
     std::shared_ptr<ie::InputInfo> input_info = network.getInputsInfo().begin()->second;
@@ -85,3 +83,26 @@ int main(void) {
         std::cout << id+1 <<  " : " << idx[id] << " : " << output[idx[id]]*100 << "% " << labels[idx[id]] << std::endl;
     }
 }
+
+
+// MEMO --------------------------------------------------------------------------------------------------------
+
+// To list all supported ONNX ops in a specific version and domain, use the get_supported_operators as shown in the example below:
+/*
+const std::int64_t version = 12;
+const std::string domain = "ai.onnx";
+const std::set<std::string> supported_ops = ngraph::onnx_import::get_supported_operators(version, domain);
+for(const auto& op : supported_ops)
+{
+    std::cout << op << std::endl;
+}
+*/
+
+// To determine whether a specific ONNX operator in a particular version and domain is supported by the importer, use the is_operator_supported function as shown in the example below:
+/*
+const std::string op_name = "Abs";
+const std::int64_t version = 12;
+const std::string domain = "ai.onnx";
+const bool is_abs_op_supported = ngraph::onnx_import::is_operator_supported(op_name, version, domain);
+std::cout << "Abs in version 12, domain `ai.onnx`is supported: " << (is_abs_op_supported ? "true" : "false") << std::endl;
+*/
